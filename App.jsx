5da681c1d7ff874bf2a4b9e5b1c9e63d02b395bc
Bright-Lumen <72477636+obodobright@users.noreply.github.com>
2022-08-07 23:32:12 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,19 +1,6 @@
 import React from "react";
 import "./App.css";
-//import Navbar from "./github-finder/navbar";
-//import UserItem from "./github-finder/userItem";
-//import Login from './advanced-react/login';
-//import EffectHooks from './advanced-react/useEffectHook';
-//  import Shop from './advanced-react/shopping';
-// import User from './advanced-react/useStateObbject';
-//import Fetch from './advanced-react/fetchData'
-// import New from './component/new';
-// import TweetList from './twitter/tweetList';
-// import BookList from './bookList/bookList';
-// import UseStateError from './advanced-react/useStateError';
-// import People from './advanced-react/people';
 
-//import  Component from './GF/component'
 
 import Pages from './firstproject/pages'
 function App() {
